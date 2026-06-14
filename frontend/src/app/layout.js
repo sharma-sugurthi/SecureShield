acb13ed319f9688a6eb6e0e25a04dbc1cc89aa6d
sharma-sugurthi <sugurthimanisharma@gmail.com>
2026-06-14 10:03:01 +0530

    bug: fixed the security bug of Module name error

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -17,7 +17,7 @@ export const viewport = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
